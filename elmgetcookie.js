2fd0f046acc66206a1472f6a9f4cd093ec58c9dd
sumuen <zim29144@gmail.com>
2023-07-22 01:28:56 +0800

    1

--- elmgetcookie.js
+++ elmgetcookie.js
@@ -28,87 +28,103 @@ function GetCookie() {
       sy.msg("写入" + CookieName + "Cookie失败‼️" + "缺少cookie2", "", "");
     }
     var cookie = sy.setdata(finalcookie, CookieKey);
-    sy.msg("elm账号" + Name + "刷新Cookie成功🎉", "", "");
+    getUserDetail(finalcookie, "CookieName")
+      .then((name) => {
+        // 在这里处理用户名
+        sy.msg("elm账号" + name + "刷新Cookie成功🎉", "", "");
+      })
+      .catch((err) => {
+        sy.msg("elm刷新Cookie成功🎉", "", "");
+      });
   }
 }
-function getUserDetail() {
-  return new Promise(resolve => {
+function getUserDetail(finalcookie, cookieName) {
+  // 添加参数finalcookie和cookieName
+  return new Promise((resolve) => {
     setTimeout(() => {
       try {
-        var url = { 
-          url: `https://restapi.ele.me/eus/v5/user_detail`, 
+        var url = {
+          url: `https://restapi.ele.me/eus/v5/user_detail`,
           headers: {
             Cookie: finalcookie,
-            'user-agent': 'iPhone;3.7.0;14.4;network/wifi;hasUPPay/0;pushNoticeIsOpen/0;lang/zh_CN;model/iPhone11,6;addressid/138164461;hasOCPay/0;appBuild/1017;supportBestPay/0;pv/7.8;apprpd/;ref/JDLTSubMainPageViewController;psq/7;ads/;psn/d40e5d4a33c100e8527f779557c347569b49c304|7;jdv/0|kong|t_1001226363_|jingfen|3bf5372cb9cd445bbb270b8bc9a34f00|1608439066693|1608439068;adk/;app_device/IOS;pap/JA2020_3112531|3.7.0|IOS 14.4;Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-            host: 'restapi.ele.me',
-            Connection: 'close',
+            "user-agent":
+              "iPhone;3.7.0;14.4;network/wifi;hasUPPay/0;pushNoticeIsOpen/0;lang/zh_CN;model/iPhone11,6;addressid/138164461;hasOCPay/0;appBuild/1017;supportBestPay/0;pv/7.8;apprpd/;ref/JDLTSubMainPageViewController;psq/7;ads/;psn/d40e5d4a33c100e8527f779557c347569b49c304|7;jdv/0|kong|t_1001226363_|jingfen|3bf5372cb9cd445bbb270b8bc9a34f00|1608439066693|1608439068;adk/;app_device/IOS;pap/JA2020_3112531|3.7.0|IOS 14.4;Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+            host: "restapi.ele.me",
+            Connection: "close",
           },
         };
-        }
 
-        if (finalcookie == undefined || finalcookie == "0" || finalcookie == null) {
-          sy.msg(cookieName, "未获取Cookie", '');
+        if (
+          finalcookie == undefined ||
+          finalcookie == "0" ||
+          finalcookie == null
+        ) {
+          sy.msg(cookieName, "未获取Cookie", "");
+          resolve("failed"); // 如果没有cookie，resolve Promise为'failed'
           return;
         }
-        sy.get(url, (error, response, data) => {  // 使用sy.get发送GET请求
+        sy.get(url, (error, response, data) => {
+          // 使用sy.get发送GET请求
           var obj = JSON.parse(data);
           if (response.status == 200) {
             // 处理返回的用户信息
             console.log(`获取用户信息成功: `, obj);
+            resolve(obj.username); // 解析用户名
           } else {
             // 处理错误状态
             console.log(`获取用户信息失败，状态码: `, response.status);
+            reject(`获取用户信息失败，状态码: ${response.status}`);
           }
-          resolve('done');
-        })
+        });
       } catch (err) {
-        resolve('done')
+        console.log(`Error: ${err}`);
+        reject(err); // 当捕获到错误时，reject这个Promise
       }
     });
-  })
+  });
 }
 
 function init() {
   isSurge = () => {
-    return undefined === this.$httpClient ? false : true
-  }
+    return undefined === this.$httpClient ? false : true;
+  };
   isQuanX = () => {
-    return undefined === this.$task ? false : true
-  }
+    return undefined === this.$task ? false : true;
+  };
   getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
-  }
+    if (isSurge()) return $persistentStore.read(key);
+    if (isQuanX()) return $prefs.valueForKey(key);
+  };
   setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
-  }
+    if (isSurge()) return $persistentStore.write(key, val);
+    if (isQuanX()) return $prefs.setValueForKey(key, val);
+  };
   msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body)
-    if (isQuanX()) $notify(title, subtitle, body)
-  }
-  log = (message) => console.log(message)
+    if (isSurge()) $notification.post(title, subtitle, body);
+    if (isQuanX()) $notify(title, subtitle, body);
+  };
+  log = (message) => console.log(message);
   get = (url, cb) => {
     if (isSurge()) {
-      $httpClient.get(url, cb)
+      $httpClient.get(url, cb);
     }
     if (isQuanX()) {
-      url.method = 'GET'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+      url.method = "GET";
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body));
     }
-  }
+  };
   post = (url, cb) => {
     if (isSurge()) {
-      $httpClient.post(url, cb)
+      $httpClient.post(url, cb);
     }
     if (isQuanX()) {
-      url.method = 'POST'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+      url.method = "POST";
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body));
     }
-  }
+  };
   done = (value = {}) => {
-    $done(value)
-  }
-  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+    $done(value);
+  };
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done };
 }
-sy.done()
+sy.done();
