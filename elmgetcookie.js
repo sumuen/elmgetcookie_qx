a213e6de5c57391401a5298e03ec9715e2803b75
sumuen <zim29144@gmail.com>
2023-07-22 01:48:38 +0800

    22

--- elmgetcookie.js
+++ elmgetcookie.js
@@ -28,17 +28,42 @@ function GetCookie() {
       sy.msg("写入" + CookieName + "Cookie失败‼️" + "缺少cookie2", "", "");
     }
     var cookie = sy.setdata(finalcookie, CookieKey);
-    getUserDetail(finalcookie, CookieName)
-      .then((name) => {
-        // 在这里处理用户名
-        sy.msg("elm账号" + name + "刷新Cookie成功🎉", "", "");
-      })
-      .catch((err) => {
-        sy.msg("getUserDetail失败", "", "");
-      });
+    // getUserDetail(finalcookie, CookieName)
+    //   .then((name) => {
+    //     // 在这里处理用户名
+    //     sy.msg("elm账号" + name + "刷新Cookie成功🎉", "", "");
+    //   })
+    //   .catch((err) => {
+    //     sy.msg("getUserDetail失败", "", "");
+    //   });
   }
-  sy.msg("elm账号"+name, "", "");
+  var url = {
+    url: `https://restapi.ele.me/eus/v5/user_detail`,
+    headers: {
+      Cookie: finalcookie,
+      "user-agent":
+        "iPhone;3.7.0;14.4;network/wifi;hasUPPay/0;pushNoticeIsOpen/0;lang/zh_CN;model/iPhone11,6;addressid/138164461;hasOCPay/0;appBuild/1017;supportBestPay/0;pv/7.8;apprpd/;ref/JDLTSubMainPageViewController;psq/7;ads/;psn/d40e5d4a33c100e8527f779557c347569b49c304|7;jdv/0|kong|t_1001226363_|jingfen|3bf5372cb9cd445bbb270b8bc9a34f00|1608439066693|1608439068;adk/;app_device/IOS;pap/JA2020_3112531|3.7.0|IOS 14.4;Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+      host: "restapi.ele.me",
+      Connection: "close",
+    },
+  };
+  sy.get(url, (error, response, data) => {
+    // 使用sy.get发送GET请求
+    var obj = JSON.parse(data);
+    if (response.status == 200) {
+      // 处理返回的用户信息
+      var username = obj.username;
+      
+      sy.msg(`获取用户信息成功: `, obj);
+      resolve(obj.username); // 解析用户名
+    } else {
+      // 处理错误状态
+      sy.msg(`获取用户信息失败，状态码: `, response.status);
+      reject(`获取用户信息失败，状态码: ${response.status}`);
+    }
+  sy.msg("elm账号"+username, "", "");
 
+})
 }
 function getUserDetail(finalcookie, cookieName) {
   // 添加参数finalcookie和cookieName
