6d0b003e0fd058ad0b7cf19acb5096176d906196
sumuen <zim29144@gmail.com>
2023-07-22 02:10:06 +0800

    11

--- elmuser.js
+++ elmuser.js
@@ -3,6 +3,13 @@ const cookieName = '饿了么'
 const cookieKey = 'cookie_elem'
 const sy = init()
 var cookieVal = sy.getdata(cookieKey);
+var headerscommon = {
+    'Content-Type': 'application/json',
+    'Cookie': cookieVal,
+    'Origin': 'https://tb.ele.me',
+    'host': "restapi.ele.me",
+    'User-Agent': 'iPhone;3.7.0;14.4;network/wifi;hasUPPay/0;pushNoticeIsOpen/0;lang/zh_CN;model/iPhone11,6;addressid/138164461;hasOCPay/0;appBuild/1017;supportBestPay/0;pv/7.8;apprpd/;ref/JDLTSubMainPageViewController;psq/7;ads/;psn/d40e5d4a33c100e8527f779557c347569b49c304|7;jdv/0|kong|t_1001226363_|jingfen|3bf5372cb9cd445bbb270b8bc9a34f00|1608439066693|1608439068;adk/;app_device/IOS;pap/JA2020_3112531|3.7.0|IOS 14.4;Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+  }
 console.log(cookieVal)
 getUserDetail()
 function getUserDetail() {
@@ -10,13 +17,7 @@ function getUserDetail() {
       try {
         var url = {
           url: `https://restapi.ele.me/eus/v5/user_detail`,
-          headers: {
-            Cookie: cookieVal,
-            "user-agent":
-              "iPhone;3.7.0;14.4;network/wifi;hasUPPay/0;pushNoticeIsOpen/0;lang/zh_CN;model/iPhone11,6;addressid/138164461;hasOCPay/0;appBuild/1017;supportBestPay/0;pv/7.8;apprpd/;ref/JDLTSubMainPageViewController;psq/7;ads/;psn/d40e5d4a33c100e8527f779557c347569b49c304|7;jdv/0|kong|t_1001226363_|jingfen|3bf5372cb9cd445bbb270b8bc9a34f00|1608439066693|1608439068;adk/;app_device/IOS;pap/JA2020_3112531|3.7.0|IOS 14.4;Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
-            host: "restapi.ele.me",
-            Connection: "close",
-          },
+          headers: headerscommon,
         };
         sy.get(url, (error, response, data) => {
           // 使用sy.get发送GET请求
